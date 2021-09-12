614bdc827fbab878e94f3278158597b7c2d3fc2b
GyeongEun Kim <kekim20@naver.com>
2021-09-12 22:40:52 +0900

    @ComponentScan과 @Autowired

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,15 @@
 package hello.core.member;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
+    @Autowired //ac.getBean(MemberRepository.class 와 같이 동작 // 의존관계 주입해줌
     public MemberServiceImpl(MemberRepository memberRepository) { //생성자
         this.memberRepository = memberRepository;
     }
@@ -16,5 +22,9 @@ public class MemberServiceImpl implements MemberService{
     @Override
     public Member findMember(Long id) {
         return memberRepository.findById(id);
+    }
+
+    public MemberRepository getMemberRepository () {
+        return memberRepository;
     }
 }
